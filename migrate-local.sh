8f5cc4c2e4c1772652f68393797df0163b27131f
Adam Mertz <adammertz@gmail.com>
2017-01-27 16:39:38 -0600

    screwed up model migration.

--- migrate-local.sh
+++ migrate-local.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export FLASK_CONFIG=default
-python manage.py db upgrade
-python manage.py db migrate
+export PY_BASE_PATH=/Users/adammertz/.virtualenvs/zillow
+${PY_BASE_PATH}/bin/python manage.py db upgrade
+${PY_BASE_PATH}/bin/python manage.py db migrate
 echo "DONE"
