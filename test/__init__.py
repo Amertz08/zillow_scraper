2505c8cc1371337d672d54a951ee9bb3f3dd451c
Adam Mertz <adammertz@gmail.com>
2017-01-27 23:11:39 -0600

    Added db as attribute to BaseTest.

--- test/__init__.py
+++ test/__init__.py
@@ -8,15 +8,19 @@ from app import db, create_app
 class BaseTest(TestCase):
     BASE_URL = 'http://localhost:5000/'
 
+    def __init__(self, *args, **kwargs):
+        super(BaseTest, self).__init__(*args, **kwargs)
+        self.db = db
+
     def create_app(self):
         return create_app('testing')
 
     def setUp(self):
-        db.create_all()
+        self.db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        self.db.session.remove()
+        self.db.drop_all()
 
     def test_setup(self):
         response = self.client.get(self.BASE_URL)
